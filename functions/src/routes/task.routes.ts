3a2ea24cd1309dab06f676f475ded3b04c90b3b5
Mischa Tobias <mischa.tobias4b@gmail.com>
2025-02-05 19:53:57 -0600

    add endpoints for tasks management

--- functions/src/routes/task.routes.ts
+++ functions/src/routes/task.routes.ts
@@ -1,15 +1,53 @@
 import { Router } from "express";
 import { TaskService } from "../services/task.service";
 import { validateUserId } from "../middlewares/validateUserId";
+import { Task } from "../models/Task";
 
 const router = Router();
 
 router.get("/", validateUserId, async (req, res, next) => {
   try {
-    const userId = req.query.userId as string;
+    const userId = req.userId as string;
     const tasks = await TaskService.getTasksByUserId(userId);
 
     res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", validateUserId, async (req, res, next) => {
+  try {
+    const userId = req.userId as string;
+    const task = await TaskService.createTask(userId, req.body);
+
+    res.status(201).json(task);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:taskId", validateUserId, async (req, res, next) => {
+  try {
+    const userId = req.userId as string;
+    const task = req.body.task as Task;
+
+    const updatedTask = await TaskService.updateTask(task, userId);
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:taskId", validateUserId, async (req, res, next) => {
+  try {
+    const userId = req.userId as string;
+    const taskId = req.params.taskId as string;
+
+    await TaskService.deleteTask(taskId, userId);
+
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
